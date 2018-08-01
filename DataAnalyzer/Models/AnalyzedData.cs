12842bb68e5c95193e6378267539d51da519271a
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2018-08-01 14:54:23 +0200

    Basic data analyzing

---
+++ DataAnalyzer/Models/AnalyzedData.cs
@@ -0,0 +1,20 @@
+﻿namespace DataAnalyzer.Models
+{
+    public class AnalyzedData
+    {
+        /// <summary>
+        ///     Average listeners count
+        /// </summary>
+        public double Average { get; set; }
+
+        /// <summary>
+        ///     Time without listeners
+        /// </summary>
+        public double TimeWithoutListeners { get; set; }
+
+        /// <summary>
+        ///     Listeners peak
+        /// </summary>
+        public int ListenersPeak { get; set; }
+    }
+}
--- DataAnalyzer/ViewModels/MainWindowViewModel.cs
+++ DataAnalyzer/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Caliburn.Micro;
+using DataAnalyzer.Models;
 
 namespace DataAnalyzer.ViewModels
 {
     public class MainWindowViewModel : Screen
     {
+        private readonly Dictionary<DateTime, Dictionary<TimeSpan, int>> _rawData =
+            new Dictionary<DateTime, Dictionary<TimeSpan, int>>();
 
+        public Dictionary<DateTime, AnalyzedData> AnalyzedData { get; set; } = new Dictionary<DateTime, AnalyzedData>();
+
+        /// <summary>
+        ///     Directory where data files are
+        /// </summary>
+        public string DataDirectory { get; set; } = AppDomain.CurrentDomain.BaseDirectory + "data";
+
+        /// <summary>
+        ///     File prefix before date
+        /// </summary>
+        public string FilePrefix { get; set; } = "text-";
+
+        public void Analyze()
+        {
+            LoadData();
+
+            foreach (var dayData in _rawData)
+            {
+                var analyzed = new AnalyzedData
+                {
+                    Average = dayData.Value.Average(entry => entry.Value),
+                    ListenersPeak = dayData.Value.Max(entry => entry.Value),
+                    TimeWithoutListeners = dayData.Value.Count(entry => entry.Value == 0) / (double) dayData.Value.Count,
+                };
+
+                AnalyzedData.Add(dayData.Key, analyzed);
+            }
+        }
+
+        private void LoadData()
+        {
+            foreach (var file in Directory.GetFiles(DataDirectory))
+            {
+                var fileName = Path.GetFileNameWithoutExtension(file);
+                var date = DateTime.Parse(fileName.Substring(FilePrefix.Length));
+
+                _rawData.Add(date, ParseDataFile(file));
+            }
+        }
+
+        private Dictionary<TimeSpan, int> ParseDataFile(string fullPath)
+        {
+            return File.ReadAllLines(fullPath).Select(i =>
+            {
+                var data = i.Split('\t');
+                return new KeyValuePair<TimeSpan, int>(TimeSpan.Parse(data[0]), int.Parse(data[1]));
+            }).ToDictionary(i => i.Key, i => i.Value);
+        }
     }
 }
