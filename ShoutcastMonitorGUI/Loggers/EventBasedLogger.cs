8da503f6854ca66da2b2dbef2fead69204e983df
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2018-08-17 15:35:18 +0200

    Fully working GUI

---
+++ ShoutcastMonitorGUI/Loggers/EventBasedLogger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Caliburn.Micro;
+using ShoutcastMonitorGUI.Models;
+using ShoutcastMonitorLib.Abstraction;
+
+namespace ShoutcastMonitorGUI.Loggers
+{
+    public class EventBasedLogger : IDataLogger
+    {
+        #region Fields
+
+        /// <summary>
+        ///     Event aggregator instance
+        /// </summary>
+        private readonly IEventAggregator _eventAggregator;
+
+        #endregion
+
+        public EventBasedLogger(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+        }
+
+        /// <inheritdoc cref="IDataLogger"/>
+        public void Log(int listeners)
+        {
+            _eventAggregator.PublishOnUIThread(new ListenerData
+            {
+                Time = DateTime.Now,
+                Listeners = listeners
+            });
+        }
+
+        /// <inheritdoc cref="IDataLogger"/>
+        public void Error(string message)
+        {
+            _eventAggregator.PublishOnUIThread(new ListenerData
+            {
+                Time = DateTime.Now,
+                Listeners = 0,
+                Message = message
+            });
+        }
+    }
+}
---
+++ ShoutcastMonitorGUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,94 @@
+﻿using Caliburn.Micro;
+using ShoutcastMonitorGUI.Loggers;
+using ShoutcastMonitorGUI.Models;
+using ShoutcastMonitorLib.Abstraction;
+using ShoutcastMonitorLib.Loggers;
+using ShoutcastMonitorLib.Receivers;
+
+namespace ShoutcastMonitorGUI.ViewModels
+{
+    public class MainWindowViewModel : Screen, IHandle<ListenerData>
+    {
+        #region Fields
+
+        /// <summary>
+        ///     Data logger
+        /// </summary>
+        private readonly IDataLogger _dataLogger;
+
+        /// <summary>
+        ///     Data receiver
+        /// </summary>
+        private readonly IReceiver _dataReceiver;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///     Stats URL to be getting data from
+        /// </summary>
+        public string StatsUrl { get; set; }
+
+        /// <summary>
+        ///     Data acquistition time interval
+        /// </summary>
+        public int TimeInterval { get; set; } = 120;
+
+        /// <summary>
+        ///     Is monitoring currently active
+        /// </summary>
+        public bool IsMonitoring { get; private set; }
+
+        /// <summary>
+        ///     Text on button
+        /// </summary>
+        public string ActionButtonText => IsMonitoring ? Properties.Strings.StopMonitoring : Properties.Strings.Monitor;
+
+        /// <summary>
+        ///     Listeners data
+        /// </summary>
+        public BindableCollection<ListenerData> ListenersData { get; } = new BindableCollection<ListenerData>();
+
+        #endregion
+
+        public MainWindowViewModel(IEventAggregator eventAggregator)
+        {
+            eventAggregator.Subscribe(this);
+
+            var logger = new CompositeLogger();
+            logger.Add(new TextFileLogger());
+            logger.Add(new EventBasedLogger(eventAggregator));
+
+            _dataLogger = logger;
+            _dataReceiver = new SimpleReceiver(StatsUrl, TimeInterval, _dataLogger);
+        }
+
+        public void Monitor()
+        {
+            IsMonitoring = !IsMonitoring;
+
+            if (IsMonitoring)
+            {
+                _dataReceiver.TimeInterval = TimeInterval;
+                _dataReceiver.StatsUrl = StatsUrl;
+
+                _dataReceiver.Start();
+            }
+            else
+            {
+                _dataReceiver.Stop();
+            }
+        }
+
+        #region IHandle
+
+        /// <inheritdoc />
+        public void Handle(ListenerData data)
+        {
+            ListenersData.Add(data);
+        }
+
+        #endregion
+    }
+}
