fb24af15f040c0e44d4fb2ae92d723805f82255b
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2018-08-18 18:56:38 +0200

    Dynamic maximum scale for plots

--- DataAnalyzer/ViewModels/MainWindowViewModel.cs
+++ DataAnalyzer/ViewModels/MainWindowViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Windows;
 using Caliburn.Micro;
 using DataAnalyzer.Models;
 using OxyPlot;
@@ -19,93 +18,6 @@ namespace DataAnalyzer.ViewModels
         /// </summary>
         private readonly Dictionary<DateTime, Dictionary<TimeSpan, int>> _rawData =
             new Dictionary<DateTime, Dictionary<TimeSpan, int>>();
-
-        #endregion
-
-        #region Properties
-
-        #region Summary data
-
-        /// <summary>
-        ///     Analyzed data in form on dictionary
-        /// </summary>
-        public BindableCollection<AnalyzedData> AnalyzedData { get; set; } = new BindableCollection<AnalyzedData>();
-
-        /// <summary>
-        ///     Data for the average listeners plot
-        /// </summary>
-        public BindableCollection<DataPoint> AverageListenersData { get; set; } = new BindableCollection<DataPoint>();
-
-        /// <summary>
-        ///     Data for the time without listeners plot
-        /// </summary>
-        public BindableCollection<DataPoint> TimeWithoutListenersData { get; set; } = new BindableCollection<DataPoint>();
-
-        /// <summary>
-        ///     Data for the listeners peak plot
-        /// </summary>
-        public BindableCollection<DataPoint> ListenersPeakData { get; set; } = new BindableCollection<DataPoint>();
-
-        #endregion
-
-        #region Daily data
-
-        /// <summary>
-        ///     List of dates for data
-        /// </summary>
-        public BindableCollection<DateTime> Dates { get; set; } = new BindableCollection<DateTime>();
-
-        /// <summary>
-        ///     Selected date
-        /// </summary>
-        public DateTime SelectedDate { get; set; }
-
-        /// <summary>
-        ///     Data for the selected day listeners plot
-        /// </summary>
-        public BindableCollection<DataPoint> SelectedDayData
-        {
-            get
-            {
-                if (!_rawData.ContainsKey(SelectedDate))
-                {
-                    return new BindableCollection<DataPoint>();
-                }
-
-                return new BindableCollection<DataPoint>(_rawData[SelectedDate]
-                    .Select(pair => new DataPoint(TimeSpanAxis.ToDouble(pair.Key), pair.Value)).ToList());
-            }
-        }
-
-        public int SelectedDayDataMax
-        {
-            get
-            {
-                if (!_rawData.ContainsKey(SelectedDate))
-                {
-                    return 100;
-                }
-
-                return _rawData[SelectedDate].Max(pair => pair.Value) + 3;
-            }
-        }
-
-        #endregion
-
-        /// <summary>
-        ///     Directory where data files are
-        /// </summary>
-        public string DataDirectory { get; set; } = AppDomain.CurrentDomain.BaseDirectory + "data";
-
-        /// <summary>
-        ///     File prefix before date
-        /// </summary>
-        public string FilePrefix { get; set; } = "text-";
-
-        /// <summary>
-        ///     Determine if analyze has been performed already
-        /// </summary>
-        public bool IsAnalyzed { get; private set; }
 
         #endregion
 
@@ -126,19 +38,26 @@ namespace DataAnalyzer.ViewModels
                     Date = dayData.Key,
                     Average = dayData.Value.Average(entry => entry.Value),
                     ListenersPeak = dayData.Value.Max(entry => entry.Value),
-                    TimeWithoutListeners = dayData.Value.Count(entry => entry.Value == 0) / (double) dayData.Value.Count,
+                    TimeWithoutListeners = dayData.Value.Count(entry => entry.Value == 0) / (double) dayData.Value.Count
                 };
 
                 AnalyzedData.Add(analyzed);
             }
 
+            NotifyOfPropertyChange(nameof(AverageListenersPlotMaxScale));
+            NotifyOfPropertyChange(nameof(TimeWithoutListenersPlotMaxScale));
+            NotifyOfPropertyChange(nameof(ListenersPeakPlotMaxScale));
+
             AverageListenersData.Clear();
             TimeWithoutListenersData.Clear();
             ListenersPeakData.Clear();
 
-            AverageListenersData.AddRange(AnalyzedData.Select(entry => new DataPoint(DateTimeAxis.ToDouble(entry.Date), entry.Average)));
-            TimeWithoutListenersData.AddRange(AnalyzedData.Select(entry => new DataPoint(DateTimeAxis.ToDouble(entry.Date), entry.TimeWithoutListeners * 100)));
-            ListenersPeakData.AddRange(AnalyzedData.Select(entry => new DataPoint(DateTimeAxis.ToDouble(entry.Date), entry.ListenersPeak)));
+            AverageListenersData.AddRange(AnalyzedData.Select(entry =>
+                new DataPoint(DateTimeAxis.ToDouble(entry.Date), entry.Average)));
+            TimeWithoutListenersData.AddRange(AnalyzedData.Select(entry =>
+                new DataPoint(DateTimeAxis.ToDouble(entry.Date), entry.TimeWithoutListeners * 100)));
+            ListenersPeakData.AddRange(AnalyzedData.Select(entry =>
+                new DataPoint(DateTimeAxis.ToDouble(entry.Date), entry.ListenersPeak)));
 
             IsAnalyzed = true;
         }
@@ -172,5 +91,105 @@ namespace DataAnalyzer.ViewModels
                 return new KeyValuePair<TimeSpan, int>(TimeSpan.Parse(data[0]), int.Parse(data[1]));
             }).ToDictionary(i => i.Key, i => i.Value);
         }
+
+        #region Properties
+
+        #region Summary data
+
+        /// <summary>
+        ///     Analyzed data in form on dictionary
+        /// </summary>
+        public BindableCollection<AnalyzedData> AnalyzedData { get; set; } = new BindableCollection<AnalyzedData>();
+
+        /// <summary>
+        ///     Data for the average listeners plot
+        /// </summary>
+        public BindableCollection<DataPoint> AverageListenersData { get; set; } = new BindableCollection<DataPoint>();
+
+        /// <summary>
+        ///     Data for the time without listeners plot
+        /// </summary>
+        public BindableCollection<DataPoint> TimeWithoutListenersData { get; set; } =
+            new BindableCollection<DataPoint>();
+
+        /// <summary>
+        ///     Data for the listeners peak plot
+        /// </summary>
+        public BindableCollection<DataPoint> ListenersPeakData { get; set; } = new BindableCollection<DataPoint>();
+
+        /// <summary>
+        ///     Average listeners plot maximum scale
+        /// </summary>
+        public int AverageListenersPlotMaxScale => AnalyzedData.Count > 0
+            ? (int) AnalyzedData.Max(data => data.Average) + 3
+            : 100;
+
+        /// <summary>
+        ///     Time without listeners plot maximum scale
+        /// </summary>
+        public int TimeWithoutListenersPlotMaxScale => AnalyzedData.Count > 0
+            ? (int) AnalyzedData.Max(data => data.TimeWithoutListeners) * 100 + 3
+            : 100;
+
+        /// <summary>
+        ///     Listeners peak plot maximum scale
+        /// </summary>
+        public int ListenersPeakPlotMaxScale => AnalyzedData.Count > 0
+            ? AnalyzedData.Max(data => data.ListenersPeak) + 3
+            : 100;
+
+        #endregion
+
+        #region Daily data
+
+        /// <summary>
+        ///     List of dates for data
+        /// </summary>
+        public BindableCollection<DateTime> Dates { get; set; } = new BindableCollection<DateTime>();
+
+        /// <summary>
+        ///     Selected date
+        /// </summary>
+        public DateTime SelectedDate { get; set; }
+
+        /// <summary>
+        ///     Data for the selected day listeners plot
+        /// </summary>
+        public BindableCollection<DataPoint> SelectedDayData
+        {
+            get
+            {
+                if (!_rawData.ContainsKey(SelectedDate)) return new BindableCollection<DataPoint>();
+
+                return new BindableCollection<DataPoint>(_rawData[SelectedDate]
+                    .Select(pair => new DataPoint(TimeSpanAxis.ToDouble(pair.Key), pair.Value)).ToList());
+            }
+        }
+
+        /// <summary>
+        ///     Selected day plot maximum scale
+        /// </summary>
+        public int SelectedDayPlotMaxScale => !_rawData.ContainsKey(SelectedDate)
+            ? 100
+            : _rawData[SelectedDate].Max(pair => pair.Value) + 3;
+
+        #endregion
+
+        /// <summary>
+        ///     Directory where data files are
+        /// </summary>
+        public string DataDirectory { get; set; } = AppDomain.CurrentDomain.BaseDirectory + "data";
+
+        /// <summary>
+        ///     File prefix before date
+        /// </summary>
+        public string FilePrefix { get; set; } = "text-";
+
+        /// <summary>
+        ///     Determine if analyze has been performed already
+        /// </summary>
+        public bool IsAnalyzed { get; private set; }
+
+        #endregion
     }
 }
